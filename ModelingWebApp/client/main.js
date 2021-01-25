cec27aeb5b61e478ae6cf11a2e563b983bce12c4
RaphAbiz <raphi1412@gmail.com>
2021-01-25 18:03:34 +0100

    1.0 First Demo w/ jointJS

--- ModelingWebApp/client/main.js
+++ ModelingWebApp/client/main.js
@@ -1,1 +1,15 @@
-import '../imports/body.js';
+import Vue from 'vue';
+import App from './App.vue';
+import './main.html';
+
+window.$ = require('jquery');
+window.joint = require('jointjs');
+
+Meteor.startup(() => {
+    new Vue({
+        el: '#app',
+        ...App,
+    });
+});
+
+
