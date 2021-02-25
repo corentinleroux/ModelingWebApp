dcd313fa2f7675b5a8f4c9eb98273521e8517285
corentinleroux <20160279@etud.univ-evry.fr>
2021-02-25 19:20:07 +0100

    some tests

--- ModelingWebAppQuasar/src/pages/Index.vue
+++ ModelingWebAppQuasar/src/pages/Index.vue
@@ -1,49 +1,7 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+<q-btn to="/login" color="primary" label="Nous rejoindre" class="q-mt-md" icon-right="send" size="16px" align="center" >
+      <q-tooltip content-class="bg-accent">Connecter-vous ou inscrivez-vous !</q-tooltip>
+    </q-btn>
+
+    
 </template>
-
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from '@vue/composition-api';
-
-export default defineComponent({
-  name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
-});
-</script>
